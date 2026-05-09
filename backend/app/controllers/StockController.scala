6dd8bcec791d5d06db87174650777d40ad4c306e
Szymon Mazulis <szymon.mazulis@b-tu.de>
2026-05-09 13:04:07 +0200

    feature: add stock endpoints

---
+++ backend/app/controllers/StockController.scala
@@ -0,0 +1,80 @@
+package controllers
+
+import models.Quote
+import models.Stock
+import models.errors.MarketDataError
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json
+import play.api.mvc.AbstractController
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
+import play.api.mvc.ControllerComponents
+import play.api.mvc.Result
+import services.{MarketDataService, MockMarketDataService, StockCatalog}
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+class StockController @Inject()(cc: ControllerComponents)(using ec: ExecutionContext) extends AbstractController(cc):
+
+    private val marketDataService: MarketDataService = MockMarketDataService()
+
+    def stocks(): Action[AnyContent] = Action {
+        Ok(Json.toJson(StockCatalog.all.map(stockToJson)))
+    }
+
+    def quote(symbol: String): Action[AnyContent] = Action.async {
+        marketDataService.getQuote(symbol).map {
+            case Right(quote) => Ok(quoteToJson(quote))
+            case Left(error) => marketDataErrorToResult(error)
+        }
+    }
+
+    private def stockToJson(stock: Stock): JsValue =
+        Json.obj(
+            "symbol" -> stock.symbol,
+            "name" -> stock.name
+        )
+
+    private def quoteToJson(quote: Quote): JsValue =
+        Json.obj(
+            "symbol" -> quote.symbol,
+            "price" -> quote.price,
+            "fetchedAt" -> quote.fetchedAt.toString
+        )
+
+    private def marketDataErrorToResult(error: MarketDataError): Result =
+        error match {
+            case MarketDataError.UnsupportedSymbol(symbol) =>
+                NotFound(
+                    Json.obj(
+                        "error" -> "UNSUPPORTED_SYMBOL",
+                        "message" -> s"Symbol $symbol is not supported."
+                    )
+                )
+
+            case MarketDataError.QuoteNotAvailable(symbol) =>
+                ServiceUnavailable(
+                    Json.obj(
+                        "error" -> "QUOTE_NOT_AVAILABLE",
+                        "message" -> s"Quote for symbol '$symbol' is not available."
+                    )
+                )
+
+            case MarketDataError.ExternalServiceUnavailable =>
+                ServiceUnavailable(
+                    Json.obj(
+                        "error" -> "EXTERNAL_SERVICE_UNAVAILABLE",
+                        "message" -> "External market data service is unavailable."
+                    )
+                )
+
+            case MarketDataError.MissingApiKey =>
+                ServiceUnavailable(
+                    Json.obj(
+                        "error" -> "MISSING_API_KEY",
+                        "message" -> "Market data API key is not configured."
+                    )
+                )
+        }
